bf7d15ac67a41219893e3b956c5d8d2e839cc56e
katsapa <mramos@gmail.com>
2026-01-13 01:35:40 +0300

    Controller realization

--- src/main/java/katsapa/spring/productservice/api/ProductController.java
+++ src/main/java/katsapa/spring/productservice/api/ProductController.java
@@ -1,11 +1,11 @@
 package katsapa.spring.productservice.api;
 
+import katsapa.spring.productservice.domain.db.ProductEntity;
 import katsapa.spring.productservice.domain.service.DbProductService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -15,7 +15,6 @@ public class ProductController {
     private final ProductDtoMapper mapper;
 
 
-
     public ProductController(DbProductService dbProductService, ProductDtoMapper mapper) {
         this.dbProductService = dbProductService;
         this.mapper = mapper;
@@ -23,8 +22,45 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<ProductDto> create(
-
+        @RequestBody ProductCreateRequest request
     ){
         log.info("Creating product with cacheMode={}", "non");
+        ProductEntity product = dbProductService.create(request);
+        ProductDto dto = mapper.toProductDto(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getById(
+            @PathVariable Long id
+    ){
+        log.info("Getting product {} with cacheMode={}", id, "non");
+        ProductEntity productEntity = dbProductService.getById(id);
+        ProductDto product = mapper.toProductDto(productEntity);
+        return  ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> update(
+            @PathVariable Long id,
+            @RequestBody ProductUpdateRequest request
+    ){
+        log.info("Updating product {} with cacheMode={}", id, "non");
+        ProductEntity entity = dbProductService.update(id, request);
+        ProductDto dto = mapper.toProductDto(entity);
+
+        return  ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long id
+    ){
+        log.info("Deleting product {} with cacheMode={}", id, "non");
+
+        dbProductService.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
